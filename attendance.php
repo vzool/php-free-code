12b77a0491c5b1fc1abf26b53173dd786a3f70e7
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2016-01-03 16:27:52 +0300

    Enhance The Data by removing holidays

--- attendance.php
+++ attendance.php
@@ -1,15 +1,29 @@
+<meta charset='utf-8'>
 <?
 
 $first_day_in_month_text = date("Y-m-01 00:00:00");
 $first_day_in_month = strtotime($first_day_in_month_text);
 
-$last_day_in_month_text = date("Y-m-t 23:59:59");
+$last_day_in_month_text = date("Y-12-31 23:59:59");
 $last_day_in_month = strtotime($last_day_in_month_text);
 
- echo "<p>first_day_in_month($first_day_in_month_text): $first_day_in_month</p>";
- echo "<p>last_day_in_month($last_day_in_month_text): $last_day_in_month</p>";
+echo "<center><h1>بيانات عشوائية لأوقات عشوائية منطقية تحاكي حضور موظف خلال عام كامل</h1></center>";
+echo "<center><h1>Random Logical Simulation for an Employee's Attendance in SQL Data</h1></center>";
+
+ echo "<p>First Day:($first_day_in_month_text): UnixTime($first_day_in_month)</p>";
+ echo "<p>Last Day:($last_day_in_month_text): UnixTime($last_day_in_month)</p>";
 
 echo "<hr/>";
+
+$day_works = array(
+	"Saturday",
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	/*"Friday",*/
+);
 
 $sql_bone = "([date], [eid], [ename], [ent], [ex])";
 
@@ -42,27 +56,30 @@ $sql_rows = [];
 
 for($day = $first_day_in_month; $day <= $last_day_in_month;  $day += 86400){
 
+	if(in_array(date("l", $day), $day_works)){
+		
+		$random_morning = complete_digits(rand(8, 9));
+		$random_miunte  = complete_digits(rand(0, 59));
+		$random_evening = complete_digits(rand(15, 17));
 
-	$random_morning = complete_digits(rand(8, 9));
-	$random_miunte  = complete_digits(rand(0, 59));
-	$random_evening = complete_digits(rand(15, 17));
+		$random_second = complete_digits(rand(0, 59));
 
-	$random_second = complete_digits(rand(0, 59));
+		$job_day_start = "$random_morning:$random_miunte:$random_second";
+		
+		$random_miunte = complete_digits(rand(0, 59));
+		$random_second = complete_digits(rand(0, 59));
 
-	$job_day_start = "$random_morning:$random_miunte:$random_second";
-	
-	$random_miunte = complete_digits(rand(0, 59));
-	$random_second = complete_digits(rand(0, 59));
+		$job_day_finish = "$random_evening:$random_miunte:$random_second";
+		
+		$date = date("Y-m-d", $day);
+		$ent = date("Y-m-d $job_day_start", $day);
+		$ex = date("Y-m-d $job_day_finish", $day);
 
-	$job_day_finish = "$random_evening:$random_miunte:$random_second";
-	
-	$date = date("Y-m-d", $day);
-	$ent = date("Y-m-d $job_day_start", $day);
-	$ex = date("Y-m-d $job_day_finish", $day);
+		$sql_rows[] = str_replace(
+			[ '[date]', '[eid]', '[ename]',    '[ent]',    '[ex]'],
+			["'$date'",       1,  "'Aziz'",   "'$ent'",    "'$ex'"], $sql_bone);
+	}
 
-	$sql_rows[] = str_replace(
-		[ '[date]', '[eid]', '[ename]',    '[ent]',    '[ex]'],
-		["'$date'",       1,  "'Aziz'",   "'$ent'",    "'$ex'"], $sql_bone);
 }
 
 $sql .= join(",\r\n", $sql_rows) . ";";
